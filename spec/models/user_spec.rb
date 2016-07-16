51f632cc2ff71c1617583230a5b6fee89c73a2a9
aleksandr <aleksandrshestakov1@gmail.com>
2016-07-16 19:03:18 +0300

    FF

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe User, type: :model do
     it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should be_valid }
+  it { should_not be_admin }
   
 it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
@@ -19,6 +22,15 @@ it { should respond_to(:password_digest) }
 it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "remember token" do
     before { @user.save }
